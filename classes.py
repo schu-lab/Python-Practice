707f956bfc2424cbb6c346be832c159e84c1ad79
s.chu <simon.chu010@gmail.com>
2021-02-13 11:36:30 -0800

    First Commit

---
+++ classes.py
@@ -0,0 +1,11 @@
+class Point():
+    #automatically called everytime we create a new Point
+    #stored inside object itself: self
+    #point in x and y coordinate system
+    def __init__(self, input1, input2):
+        self.x = input1
+        self.y = input2
+
+p = Point(2, 8)
+print(p.x)
+print(p.y)
