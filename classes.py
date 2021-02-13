869cb2f49390caeec9ac5524f0b03b090f8f84e6
s.chu <simon.chu010@gmail.com>
2021-02-13 11:44:28 -0800

    Added 3 functions to Flight class

--- classes.py
+++ classes.py
@@ -16,4 +16,22 @@ class Flight():
         self.capacity = capacity
         self.passenger = []
 
+    def add_passenger(self, name):
+        # essentially == 0
+        if not self.open_seats():
+            return False
+        self.passengers.append(name)
+        return True
+
+    def open_seats(self):
+        return self.capacity - len(self.passenger)
+
 flight = Flight(3)
+
+people = ["Harry", "Ron", "Hermione", "Ginny"]
+for person in people:
+    sucess = flight.add_passenger(person)
+    if sucess:
+        print(f"Added {person} to flight sucessfully.")
+    else:
+        print(f"No avaliable seats for {person}")
