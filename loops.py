812b22fed3f1e7e7d903c36919785a177d647579
s.chu <simon.chu010@gmail.com>
2021-02-13 11:07:40 -0800

    First Commit

--- loops.py
+++ loops.py
@@ -1,2 +1,17 @@
 for i in [0, 1, 2, 3, 4, 5]:
     print(i)
+
+for i in range(6):
+    print(i)
+
+#Print names in list:
+names = ["Harry", "Ron", "Hermione"]
+
+for name in names:
+    print(name)
+
+#Print characters in string:
+name = "Harry"
+
+for character in name:
+    print(character)
