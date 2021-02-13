023e833aa138479477bbc578b6edd7e222ba1305
s.chu <simon.chu010@gmail.com>
2021-02-13 10:50:00 -0800

    First Commit

--- sequences.py
+++ sequences.py
@@ -1,3 +1,14 @@
 name = "Harry"
-
 print(name[0])
+
+multiname = ["Harry", "Ron", "Hermione"]
+print(multiname)
+print(multiname[0])
+
+coordinateX = 10.0
+coordinateY = 20.0
+
+coordinate = (10.0, 20.0)
+
+print(coordinate)
+print(coordinate[1])
